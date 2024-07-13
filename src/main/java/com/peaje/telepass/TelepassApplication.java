5e76ebd09e539d89e799f83dee94608183cdab55
cdquinterop <darmiqp@gmail.com>
2024-07-13 13:02:30 -0500

    Estructura del proyecto

---
+++ src/main/java/com/peaje/telepass/TelepassApplication.java
@@ -0,0 +1,13 @@
+package com.peaje.telepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelepassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelepassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peaje/telepass/TelepassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaje.telepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelepassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
