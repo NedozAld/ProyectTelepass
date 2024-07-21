b1dc0c01ff9d29f05276b80b4fa99fcde28d35da
NedozAld <119252358+NedozAld@users.noreply.github.com>
2024-07-21 16:35:37 -0500

    Update SecurityConfig.java

--- src/main/java/com/peaje/telepass/Security/SecurityConfig.java
+++ src/main/java/com/peaje/telepass/Security/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.peaje.telepass.Security;
-
+//SecurityConfig
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
